c55de2dc4f9eb7c47a41af040b291447e673422f
maksimkir <kiriluk157@gmail.com>
2025-03-03 10:12:31 +0200

    add class FulltimeEmployee

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 class Employee{
-private:
+protected:
     int id;
     double salary;
 public:
@@ -14,5 +14,17 @@ public:
     }
     double calculate() const {
         return salary;
+    }
+};
+class FulltimeEmployee : public Employee {
+public:
+    double bonus;
+public:
+    FulltimeEmployee(int id,double salary , double bonus):Employee(id ,salary), bonus(bonus) {}
+    void showInfo() const {
+        cout<<"Full-Time Employee: "<< id<<endl<<"Salaty: "<<salary<<endl<<"Bonus: "<<bonus<<endl;
+    }
+    double calculate() const {
+        return salary + bonus;
     }
 };
